2d9e6420becd5213cb60b5abf9218e7a93e35ab1
Jack Dench <jdench1989@gmail.com>
2025-04-02 10:23:05 +0100

    changes to before install script

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 # Exit on error
-sudo set -e
+set -e
 
 # Install Node.js properly (using nodesource)
-sudo curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
-sudo yum install -y nodejs
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
+yum install -y nodejs
 
 # Install dependencies
-sudo npm install
+npm ci --unsafe-perm
 
 # Install MongoDB
-sudo echo "[mongodb-org-8.0]
+echo "[mongodb-org-8.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://pgp.mongodb.com/server-8.0.asc" > /etc/yum.repos.d/mongodb-org-8.0.repo
+gpgkey=https://pgp.mongodb.com/server-8.0.asc" | tee /etc/yum.repos.d/mongodb-org-8.0.repo
 
-sudo yum install -y mongodb-org
-sudo systemctl start mongod
+yum install -y mongodb-org
+
+# Ensure MongoDB service is correctly set up
+systemctl daemon-reload
+systemctl enable mongod
+systemctl start mongod
